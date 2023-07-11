ba9f5742ad9ebdbc676d29edd9818469df4eeb22
Mykola Krasko <mikokrsk@gmail.com>
2023-07-11 23:45:09 +0300

    Fixed all Errors

--- Assets/CongratScript.cs
+++ Assets/CongratScript.cs
@@ -1,31 +1,33 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class CongratScript : MonoBehaviour
 {
     public TextMesh Text;
     public ParticleSystem SparksParticles;
-    
+
     private List<string> TextToDisplay;
-    
+
     private float RotatingSpeed;
     private float TimeToNextText;
 
     private int CurrentText;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         TimeToNextText = 0.0f;
-        CurrentText = 0
-        
-        RotatingSpeed = 1.0;
+        CurrentText = 0;
 
-        TextToDisplay.Add("Congratulation");
-        TextToDisplay.Add("All Errors Fixed");
+        RotatingSpeed = 1.0f;
+        TextToDisplay = new List<string>
+        {
+            "Congratulation",
+            "All Errors Fixed"
+        };
 
         Text.text = TextToDisplay[0];
-        
+
         SparksParticles.Play();
     }
 
@@ -34,17 +36,19 @@ public class CongratScript : MonoBehaviour
     {
         TimeToNextText += Time.deltaTime;
 
+        SparksParticles.gameObject.transform.Rotate(Text.transform.position.normalized, RotatingSpeed);
+
         if (TimeToNextText > 1.5f)
         {
-            TimeToNextText = 0.0f;
-            
             CurrentText++;
             if (CurrentText >= TextToDisplay.Count)
             {
                 CurrentText = 0;
+            }
 
-
+            TimeToNextText = 0.0f;
             Text.text = TextToDisplay[CurrentText];
         }
     }
+
 }
